23413b97a74174d3a8f1d439849e493c869c08e5
clearloop <26088946+clearloop@users.noreply.github.com>
2023-09-07 12:51:28 +0800

    feat(service): support download

--- src/service.rs
+++ src/service.rs
@@ -1,16 +1,17 @@
 use actix_cors::Cors;
 use actix_multipart::Multipart;
-use actix_web::{http::header, post, web, web::Bytes, App, Error, HttpResponse, HttpServer};
+use actix_web::{
+    get, http::header, post, web, web::Bytes, App, Error, HttpRequest, HttpResponse, HttpServer,
+};
 use anyhow::Result;
 use futures_util::StreamExt as _;
 use std::{fs::File, io::Write, path::PathBuf};
 
 /// Save file to directory
-async fn save_file(bytes: Vec<Bytes>, path: PathBuf) -> anyhow::Result<()> {
-    let filepath = format!("{:?}", path);
-
+async fn save_file(bytes: Vec<Bytes>, path: &PathBuf) -> anyhow::Result<()> {
+    let path = path.clone();
     // File::create is blocking operation, use threadpool
-    let mut f = web::block(|| File::create(filepath)).await??;
+    let mut f = web::block(|| File::create(path)).await??;
 
     // Field in turn is stream of *Bytes* object
     for data in bytes {
@@ -19,6 +20,11 @@ async fn save_file(bytes: Vec<Bytes>, path: PathBuf) -> anyhow::Result<()> {
     }
 
     Ok(())
+}
+
+/// Train model
+async fn train(path: &PathBuf) -> anyhow::Result<PathBuf> {
+    Ok(path.clone())
 }
 
 /// greeting
@@ -44,10 +50,28 @@ async fn upload(_artist: web::Path<String>, mut payload: Multipart) -> Result<Ht
         return Ok(HttpResponse::BadRequest().into());
     }
 
-    save_file(bytes, "source.wav".into())
+    let path = PathBuf::from("source.wav");
+    save_file(bytes, &path).await.expect("save file error");
+
+    let output = train(&path)
         .await
-        .expect("save file error");
-    Ok(HttpResponse::Ok().into())
+        .expect("train error")
+        .to_string_lossy()
+        .to_string();
+
+    Ok(HttpResponse::Ok()
+        .content_type(header::ContentType::plaintext())
+        .insert_header(("X-Hdr", "sample"))
+        .body(format!("/download/{output}")))
+}
+
+/// Download file
+#[get("/download/{name}")]
+async fn download(req: HttpRequest, audio: web::Path<String>) -> Result<HttpResponse, Error> {
+    println!("download: {:?}", audio);
+    let path = audio.to_string();
+    let file = actix_files::NamedFile::open_async(PathBuf::from(path)).await?;
+    Ok(file.into_response(&req))
 }
 
 /// start service
@@ -60,7 +84,7 @@ pub async fn start(port: u16) -> anyhow::Result<()> {
             .allowed_header(header::CONTENT_TYPE)
             .max_age(3600);
 
-        App::new().wrap(cors).service(upload)
+        App::new().wrap(cors).service(upload).service(download)
     })
     .bind(("0.0.0.0", port))?
     .run()
