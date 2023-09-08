69ae8182d6b10b4420eb737c4a92f069a0620d25
clearloop <26088946+clearloop@users.noreply.github.com>
2023-09-08 18:27:04 +0800

    feat(tls): remove ssl

--- src/service.rs
+++ src/service.rs
@@ -5,7 +5,6 @@ use actix_web::{
 };
 use anyhow::Result;
 use futures_util::StreamExt as _;
-use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 use std::{fs::File, io::Write, path::PathBuf, process::Command};
 
 enum Artist {
@@ -153,10 +152,6 @@ async fn download(req: HttpRequest, audio: web::Path<String>) -> Result<HttpResp
 
 /// start service
 pub async fn start(port: u16) -> anyhow::Result<()> {
-    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
-    builder.set_private_key_file("key.pem", SslFiletype::PEM)?;
-    builder.set_certificate_chain_file("cert.pem")?;
-
     HttpServer::new(|| {
         let cors = Cors::default()
             .allow_any_origin()
@@ -167,7 +162,7 @@ pub async fn start(port: u16) -> anyhow::Result<()> {
 
         App::new().wrap(cors).service(upload).service(download)
     })
-    .bind_openssl("127.0.0.1:8080", builder)?
+    .bind(("0.0.0.0", port))?
     .run()
     .await
     .map_err(Into::into)
